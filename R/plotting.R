9dcaf4d34971eb512081b858300eee1644161a2e
Bo Peng <ben.bob@gmail.com>
2021-02-22 13:16:08 -0600

    Test change figure

--- R/plotting.R
+++ R/plotting.R
@@ -17,7 +17,7 @@ plot_model <- function(output, linesize, textsize, xlabel, ylabel, legend_title,
 
     print(rownames(output))
     print(colnames(output))
-
+    output = output[output$variable == 'I', ]
     output$variable <- factor(output$variable, levels = levels)
 
     ggplot(output, aes(x = time, y = value, colour = as.factor(variable))) +
